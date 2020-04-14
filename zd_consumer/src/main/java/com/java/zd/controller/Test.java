36d8d6273dea70e6cb55419414466198ed107ffe
jingjingLee567 <boelin88@hotmail.com>
2020-04-14 15:21:22 +0800

    Revert: 删除test第一次

---
+++ zd_consumer/src/main/java/com/java/zd/controller/Test.java
@@ -0,0 +1,14 @@
+package com.java.zd.controller;
+
+/**
+ * description:
+ * author:JJ Lee
+ * time:2020/4/14 14:25
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("添加一个新的类");
+    }
+}
